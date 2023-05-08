89542530c7acc1df54f013f91f7ab459d5e753d4
Matthew Zmija <matthewzmija@Monicas-MacBook-Air.local>
2023-05-08 17:06:11 -0400

    Added code to generate prompt and README file

---
+++ GeneratedREADME/README.md
@@ -0,0 +1,31 @@
+# Professional README Generator ![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)
+
+## Description
+This application allows a developer who is working on a project, to generate a professional README file in a timely manner
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+- [Contact](#contact)
+
+## Installation
+The user is required to download inquirer and fs
+
+## Usage
+The application's usage takes place in the terminal
+
+## Contribution
+Matthew Zmija
+
+## Tests
+node 'file name' (in this case, it is index.js)
+
+## License
+![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)
+
+## Contact
+My GitHub is https://github.com/matthewzmija and my email is M.Zmija.12@gmail.com
--- index.js
+++ index.js
@@ -1,2 +1,101 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please provide a detailed description of your project",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What is required of the user to install/run this application?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Provide some instructions on how the application is utilized",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What license would you like to use?",
+      choices: ["MIT", "GNU General Public License v3.0", "Apache License 2.0"],
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "Who are the contributors to this project?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What command is required to test your application?",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "Please provide your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please provide your email",
+    },
+  ])
+  .then((data) => {
+    let licenseBadge = "";
+    if (data.license == "MIT") {
+      licenseBadge =
+        "![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)";
+    }
+    if (data.license == "GNU General Public License v3.0") {
+      licenseBadge =
+        "![License: GNU General Public License v3.0](https://img.shields.io/badge/License-GNU%20General%20Public%20License%20v3.0-blue.svg)";
+    }
+    if (data.license == "Apache License 2.0") {
+      licenseBadge ==
+        "![License: Apache License 2.0](https://img.shields.io/badge/License-Apache%20License%202-blue.svg)";
+    }
+    const readMeFile = `# ${data.title} ${licenseBadge}
+
+## Description
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+- [Contact](#contact)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contribution
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## License
+${licenseBadge}
+
+## Contact
+My GitHub is https://github.com/${data.username} and my email is ${data.email}`;
+    fs.writeFile("./GeneratedREADME/README.md", readMeFile, (err) =>
+      err ? console.log(err) : console.log("Success!")
+    );
+  });
